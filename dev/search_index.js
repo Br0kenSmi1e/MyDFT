bd7603344e9ebec64d3ca93b68beba592df72982
Documenter.jl <documenter@juliadocs.github.io>
2024-10-11 12:03:17 +0000

    build based on dec20a2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyDFT","category":"page"},{"location":"#MyDFT","page":"Home","title":"MyDFT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyDFT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyDFT]","category":"page"}]
+}
